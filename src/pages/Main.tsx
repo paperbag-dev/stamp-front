a41a8c7cf610bc6bfb804cc11b4a2b874b0aae7b
maitracle <maitracle@gmail.com>
2019-09-22 20:08:00 +0900

    CHG: 반응형 스타일 적용 mixin 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -8,9 +8,14 @@ import './style/Main.scss';
 export default class Main extends React.Component {
   render() {
     return (
-      <div className="page-wrapper">
-        <h1>main page</h1>
-        <TestComponentOne />
+      <div className={"page-wrapper"}>
+        <h1 className={"page-title"}>main page</h1>
+        <div className={"component-wrapper"}>
+          <div className={"item"}><TestComponentOne /></div>
+          <div className={"item"}><TestComponentOne /></div>
+          <div className={"item"}><TestComponentOne /></div>
+          <div className={"item"}><TestComponentOne /></div>
+        </div>
       </div>
     )
   }
