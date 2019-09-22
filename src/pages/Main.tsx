74de474c2cf59d6f241e4c33c8ebf29531ef1249
maitracle <maitracle@gmail.com>
2019-09-22 19:34:39 +0900

    CHG: node-scss 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import TestComponentOne from 'components/TestComponentOne';
 
+import './style/Main.scss';
+
 
 export default class Main extends React.Component {
   render() {
     return (
-      <div>
+      <div className="page-wrapper">
         <h1>main page</h1>
         <TestComponentOne />
       </div>
